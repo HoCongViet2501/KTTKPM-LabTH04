a8082f6c2f784fdee84cffa2183fe613baf7c528
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-09-19 13:01:02 +0700

    first commit

---
+++ src/main/java/com/se/kttkpmlabth04/KttkpmLabTh04Application.java
@@ -0,0 +1,13 @@
+package com.se.kttkpmlabth04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KttkpmLabTh04Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(KttkpmLabTh04Application.class, args);
+    }
+    
+}
