60b8cc7b935574fa894e191f6137644b6452ef45
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-09-19 14:41:02 +0700

    finish cau2

--- src/test/java/com/se/kttkpmlabth04/ChuyenBayTest.java
+++ src/test/java/com/se/kttkpmlabth04/ChuyenBayTest.java
@@ -4,9 +4,10 @@ import com.se.kttkpmlabth04.entity.ChuyenBay;
 import com.se.kttkpmlabth04.repository.ChuyenBayRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ChuyenBayTest {
     @Autowired
     private ChuyenBayRepository chuyenBayRepository;
